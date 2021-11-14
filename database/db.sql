c22ff2a187104ee1b748f96047848b77b0237d7f
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2021-11-14 14:52:01 -0300

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,40 +1,40 @@
 CREATE TABLE Location(
-	ID INT PRIMARY KEY,
-	name VARCHAR(64) NOT NULL,
-	type VARCHAR(64),
-	dimension VARCHAR(64),
-	created_at VARCHAR(64) NOT NULL
+    ID INT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    type VARCHAR(64),
+    dimension VARCHAR(64),
+    created_at VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE Episode(
-	ID INT PRIMARY KEY,
-	name VARCHAR(64) NOT NULL,
-	air_date VARCHAR(64),
-	episode_code VARCHAR(64),
-	created_at VARCHAR(64) NOT NULL
+    ID INT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    air_date VARCHAR(64),
+    episode_code VARCHAR(64),
+    created_at VARCHAR(64) NOT NULL
 );
 
 CREATE TYPE type_character AS ENUM('Alive', 'Dead', 'unknown');
 CREATE TYPE type_gender AS ENUM('Female', 'Male', 'Genderless', 'unknown');
 CREATE TABLE Character(
-	ID INT PRIMARY KEY,
-	name VARCHAR(64) NOT NULL,
-	status type_character,
-	specie VARCHAR(64),
-	subspecies VARCHAR(64),
-	gender type_gender,
-	id_origin INT,
-	id_last_location INT,
-	created_at VARCHAR(64) NOT NULL,
-	FOREIGN KEY (id_origin) REFERENCES Location(ID),
-	FOREIGN KEY (id_last_location) REFERENCES Location(ID)
+    ID INT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    status type_character,
+    specie VARCHAR(64),
+    subspecie VARCHAR(64),
+    gender type_gender,
+    created_at VARCHAR(64) NOT NULL,
+    id_origin INT,
+    id_last_location INT,
+    FOREIGN KEY (id_origin) REFERENCES Location(ID),
+    FOREIGN KEY (id_last_location) REFERENCES Location(ID)
 );
 
 CREATE TABLE Appear(
-	ID SERIAL PRIMARY KEY,
-	character_id INT NOT NULL,
-	episode_id INT NOT NULL,
-	FOREIGN KEY (character_id) REFERENCES Character(ID),
-	FOREIGN KEY (episode_id) REFERENCES Episode(ID),
-	UNIQUE(character_id, episode_id)
+    ID SERIAL PRIMARY KEY,
+    character_id INT NOT NULL,
+    episode_id INT NOT NULL,
+    FOREIGN KEY (character_id) REFERENCES Character(ID),
+    FOREIGN KEY (episode_id) REFERENCES Episode(ID),
+    UNIQUE(character_id, episode_id)
 );
