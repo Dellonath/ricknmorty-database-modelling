ebac229d65e232e7a43b79923b25cde44457c90d
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2021-12-13 00:41:49 -0300

    Changing attributes namtes

--- database/db.sql
+++ database/db.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Location(
-    ID INT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    type VARCHAR(64),
+    location_id INT PRIMARY KEY,
+    location_name VARCHAR(64) NOT NULL,
+    location_type VARCHAR(64),
     dimension VARCHAR(64),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Episode(
-    ID INT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
+    episode_id INT PRIMARY KEY,
+    episode_name VARCHAR(64) NOT NULL,
     air_date VARCHAR(64),
     episode_code VARCHAR(64),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -17,8 +17,8 @@ CREATE TABLE Episode(
 CREATE TYPE type_character AS ENUM('Alive', 'Dead', 'unknown');
 CREATE TYPE type_gender AS ENUM('Female', 'Male', 'Genderless', 'unknown');
 CREATE TABLE Character(
-    ID INT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
+    character_id INT PRIMARY KEY,
+    character_name VARCHAR(64) NOT NULL,
     status type_character,
     specie VARCHAR(64),
     subspecie VARCHAR(64),
@@ -31,23 +31,10 @@ CREATE TABLE Character(
 );
 
 CREATE TABLE Appear(
-    ID SERIAL PRIMARY KEY,
-    character_id INT NOT NULL,
-    episode_id INT NOT NULL,
-    FOREIGN KEY (character_id) REFERENCES Character(ID),
-    FOREIGN KEY (episode_id) REFERENCES Episode(ID),
-    UNIQUE(character_id, episode_id)
+    appear_id SERIAL PRIMARY KEY,
+    character_id_fk INT NOT NULL,
+    episode_id_fk INT NOT NULL,
+    FOREIGN KEY (character_id_fk) REFERENCES Character(character_id),
+    FOREIGN KEY (episode_id_fk) REFERENCES Episode(episode_id),
+    UNIQUE(character_id_fk, episode_id_fk)
 );
-
-CREATE ROLE superuser;
-GRANT ALL PRIVILEGES ON DATABASE "ricknmorty" TO superuser;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO superuser;
-
-CREATE USER master WITH PASSWORD 'senhamaster123';
-GRANT superuser TO master;
-
-CREATE USER usr WITH PASSWORD 'senhausr123';
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO usr;
-GRANT USAGE ON SCHEMA public TO usr;
-
-CREATE INDEX character_specie_index ON character(specie);
