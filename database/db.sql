245cf5a18a5f8a4aef4b906ed5c173fa8468bdeb
Douglas Oliveira <56659549+Dellonath@users.noreply.github.com>
2021-12-13 01:04:30 -0300

    Adding Privileges and Index

--- database/db.sql
+++ database/db.sql
@@ -38,3 +38,17 @@ CREATE TABLE Appear(
     FOREIGN KEY (episode_id_fk) REFERENCES Episode(episode_id),
     UNIQUE(character_id_fk, episode_id_fk)
 );
+
+CREATE ROLE superuser;
+GRANT ALL PRIVILEGES ON DATABASE "ricknmorty" TO superuser;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO superuser;
+
+CREATE USER master WITH PASSWORD 'senhamaster123';
+
+GRANT superuser TO master;
+
+CREATE USER usr WITH PASSWORD 'senhausr123';
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO usr;
+GRANT USAGE ON SCHEMA public TO usr;
+
+CREATE INDEX character_specie_index ON character(specie);
